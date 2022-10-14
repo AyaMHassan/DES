3270c5b4b158a4cf0620c304640f307854da4764
Fatma-Badr <85190955+Fatma-Badr@users.noreply.github.com>
2022-10-14 20:22:40 +0200

    Adding keys, encryption, decryption and write a file functions

--- des.cpp
+++ des.cpp
@@ -3,6 +3,41 @@
 #include <fstream>
 using namespace std;
 int n;
+int p_c1[56] = { 57, 49, 41, 33, 25, 17, 9,  1,  58, 50, 42, 34,
+                 26, 18, 10, 2,  59, 51, 43, 35, 27, 19, 11, 3,
+                 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7,
+                 62, 54, 46, 38, 30, 22, 14, 6,  61, 53, 45, 37,
+                 29, 21, 13, 5,  28, 20, 12, 4 };
+
+int p_c2[48] = { 14, 17, 11, 24, 1,  5,  3,  28,
+                 15, 6,  21, 10, 23, 19, 12, 4,
+                 26, 8,  16, 7,  27, 20, 13, 2,
+                 41, 52, 31, 37, 47, 55, 30, 40,
+                 51, 45, 33, 48, 44, 49, 39, 56,
+                 34, 53, 46, 42, 50, 36, 29, 32 };
+
+int init_p[64] = { 58, 50, 42, 34, 26, 18, 10, 2,  60, 52, 44,
+                   36, 28, 20, 12, 4,  62, 54, 46, 38, 30, 22,
+                   14, 6,  64, 56, 48, 40, 32, 24, 16, 8,  57,
+                   49, 41, 33, 25, 17, 9,  1,  59, 51, 43, 35,
+                   27, 19, 11, 3,  61, 53, 45, 37, 29, 21, 13,
+                   5,  63, 55, 47, 39, 31, 23, 15, 7 };
+
+int inv_p[64]= { 40, 8,  48, 16, 56, 24, 64, 32, 39, 7,  47,
+                 15, 55, 23, 63, 31, 38, 6,  46, 14, 54, 22,
+                 62, 30, 37, 5,  45, 13, 53, 21, 61, 29, 36,
+                 4,  44, 12, 52, 20, 60, 28, 35, 3,  43, 11,
+                 51, 19, 59, 27, 34, 2,  42, 10, 50, 18, 58,
+                 26, 33, 1,  41, 9,  49, 17, 57, 25 };
+
+int exp_[48] = { 32, 1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,
+                8,  9,  10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
+                16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
+                24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1 };
+
+int per[32] = { 16, 7, 20, 21, 29, 12, 28, 17, 1,  15, 23,
+                26, 5, 18, 31, 10, 2,  8,  24, 14, 32, 27,
+                3,  9, 19, 13, 30, 6,  22, 11, 4,  25 };
 int sbox_arr[8][4][16]={
     {
        {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
@@ -53,6 +88,10 @@ int sbox_arr[8][4][16]={
         {2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}
     }
 };
+uint64_t get_bits(uint64_t bits, uint8_t pos, uint64_t mask)
+{
+    return (bits >> pos) & mask;
+}
 uint64_t set_bit_original (uint64_t bits, uint8_t pos, uint8_t value)
 {
    uint64_t mask = 1LL << (63 - pos);
@@ -201,6 +240,134 @@ uint64_t* read(string content)
         }
     }
     return plaintext;
+}
+char hex_table(uint64_t n) {
+    if (n == 0) return '0';
+    else if (n == 1) return '1';
+    else if (n == 2) return '2';
+    else if (n == 3) return '3';
+    else if (n == 4) return '4';
+    else if (n == 5) return '5';
+    else if (n == 6) return '6';
+    else if (n == 7) return '7';
+    else if (n == 8) return '8';
+    else if (n == 9) return '9';
+    else if (n == 10) return 'A';
+    else if (n == 11) return 'B';
+    else if (n == 12) return 'C';
+    else if (n == 13) return 'D';
+    else if (n == 14) return 'E';
+    else if (n == 15) return 'F';
+    
+}
+
+string convert_to_hex(uint64_t num) {
+    string hex = "";
+    for (int i = 60; i > -1; i--) {
+        hex += hex_table(get_bits(num, i, 0b1111));
+        i = i - 3;
+    }
+    return hex;
+}
+
+void output_file(string path, string out) {
+    string p;
+    for (int i = 0; i < path.size(); i++) {
+        if (path[i] == '\\') {
+            p = path + "\\output.txt";
+            break;
+        }
+        else if (path[i] == '/') {
+            p = path + "/output.txt";
+            break;
+        
+        }
+
+    }
+    cout << p << endl;
+    ofstream output;
+    output.open(p);
+    if (output.is_open())
+    {
+        output << out;
+
+        output.close();
+    }
+    else cout << "File opening is fail";
+}
+uint64_t* keys(uint64_t key) {
+    //cout << key << endl;
+    static uint64_t keys[16];
+    uint64_t pc1= permutation(p_c1,key,64,56);
+    //cout << "pc1  " << pc1 << endl;
+    uint64_t key_right = get_bits(pc1, 0, 0xfffffff);
+    //cout << "key_right  " << key_right << endl;
+    uint64_t key_left = get_bits(pc1, 28, 0xfffffff);
+    //cout << "key_left  " << key_left << endl;
+    
+    for (int i = 1; i < 17; i++) {
+        if ((i == 1) || (i == 2) || (i == 9) || (i == 16)) {
+            key_right = shift_left_1(key_right);
+            key_left = shift_left_1(key_left);
+        }
+        else {
+            key_right = shift_left_2(key_right);
+            key_left = shift_left_2(key_left);
+        }
+        //cout << "key_right  " <<i<<":   " << key_right << endl;
+       // cout << "key_left  " << i << ":   " << key_left << endl;
+        uint64_t key_con= (key_right|(key_left << 28));
+        //cout << "key_con  " << i << ":   " << key_con << endl;
+        uint64_t key_round= permutation(p_c2, key_con, 56,48);
+        //cout << "key_round  " << i << ":   " << key_round << endl;
+        keys[i-1] = key_round;
+
+    }
+    return keys;
+}
+uint64_t* decrp_keys(uint64_t key) {
+    uint64_t* k = keys(key);
+    static uint64_t rkeys[16];
+    for (int i = 0; i < 16; i++) {
+        rkeys[i] = k[15 - i];
+    }
+    return rkeys;
+}
+
+uint64_t f_function(uint64_t n, uint64_t k) {
+    uint64_t n_exp= permutation(exp_, n,32, 48);
+    //cout << "n_exp  " << ":   " << n_exp << endl;
+    uint64_t n_xor = xor_logic(n_exp, k);
+    //cout << "n_xor  " << ":   " <<k<<"    " << n_xor << endl;
+    uint64_t n_sbox = sbox(n_xor);
+   // cout << "n_sbox  " << ":   " << n_sbox << endl;
+    return permutation(per, n_sbox, 32,32);
+}
+
+uint64_t des(uint64_t seq, uint64_t key,int t) {
+    uint64_t* keys_;
+    if (t == 0) keys_ = keys(key);
+    else keys_ = decrp_keys(key);
+    //cout << "------------" << endl;
+    uint64_t seq_ip= permutation(init_p, seq, 64,64);
+    //cout << "seq_ip  " << ":   " << seq_ip << endl;
+    uint64_t p_right= get_bits(seq_ip, 0, 0xffffffff);
+    //cout << "p_right  " << ":   " << p_right << endl;
+    uint64_t p_left = get_bits(seq_ip, 32, 0xffffffff);
+    //cout << "p_left  " << ":   " << p_left << endl;
+    for (int i = 1; i < 17; i++) {
+        uint64_t lt = p_left;
+        p_left = p_right;
+        //cout << "f_fn  " << ":   " << f_function(p_right, keys_[i - 1]) << endl;
+       // cout << "key " << i << ":   " << keys_[i - 1] << endl;
+        p_right = xor_logic(lt, f_function(p_right, keys_[i - 1]));
+       // cout << p_left << " ......    " << p_right << "tl: " <<lt << endl;
+       // cout << "round " << i << ":  " << (p_right | (p_left << 32));
+    }
+
+    uint64_t seq_swap = (p_left | (p_right << 32));
+    
+    return permutation(inv_p, seq_swap, 64,64);
 }
 //
 int main(int argc,char* argv[])
